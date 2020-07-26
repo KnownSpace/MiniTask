8b2c315d00904c273056a3406cc42d1017ddb18b
孜然 <2145973003@qq.com>
2020-07-26 20:31:55 +0800

    [Add]更新测试

--- src/main/java/org/knownspace/minitask/Test.java
+++ src/main/java/org/knownspace/minitask/Test.java
@@ -16,36 +16,31 @@ public class Test {
         UniqueFlag uniqueFlag = factory.makeUniqueFlag();
         //只出现以下中的一个
         uniqueFlag.lock().then(()->{
-            //故意不释放
-            //uniqueFlag.unlock();
+            uniqueFlag.unlock();
             System.out.println("Unique Lock 1");
         });
         uniqueFlag.lock().then(()->{
+            uniqueFlag.unlock();
             System.out.println("Unique Lock 2");
         });
 
         //信号量
         //同时允许三个线程访问
         SharedFlag sharedFlag = factory.makeSharedFlag(3);
-        //只出现以下中的三个
         sharedFlag.lock().then(()->{
-            //故意不释放
-            //sharedFlag.unlock();
+            sharedFlag.unlock();
             System.out.println("Shared Lock 1");
         });
         sharedFlag.lock().then(()->{
-            //故意不释放
-            //sharedFlag.unlock();
+            sharedFlag.unlock();
             System.out.println("Shared Lock 2");
         });
         sharedFlag.lock().then(()->{
-            //故意不释放
-            //sharedFlag.unlock();
+            sharedFlag.unlock();
             System.out.println("Shared Lock 3");
         });
         sharedFlag.lock().then(()->{
-            //故意不释放
-            //sharedFlag.unlock();
+            sharedFlag.unlock();
             System.out.println("Shared Lock 4");
         });
 
@@ -63,10 +58,8 @@ public class Test {
         });
         readWriteFlag.lockWrite().then(()->{
             System.out.println("Write 1");
-            //故意不释放
-            //readWriteFlag.unlock();
+            readWriteFlag.unlock();
         }).then(()->{
-            //绝不出现
             readWriteFlag.lockRead().then(()->{
                 System.out.println("Read 3");
             });
